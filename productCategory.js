fd729164405fa0ee522d23ff18825f870a77004c
maimunarashid <rashidromana05@gmail.com>
2025-12-19 02:57:35 +0600

    landing page done

--- productCategory.js
+++ productCategory.js
@@ -10,15 +10,15 @@ const productCategories = document.getElementById("product-categories");
 productCategories.innerHTML = "";
 
 // Category Heading create
-const categoryHeading = document.createElement("h1");
-categoryHeading.innerText = "Makeup Products";
-categoryHeading.classList.add("text-2xl", "font-bold", "mb-2", "ml-2", "text-[#E2725B]");
-productCategories.appendChild(categoryHeading);
+// const categoryHeading = document.createElement("h1");
+// categoryHeading.innerText = "Makeup Products";
+// categoryHeading.classList.add("text-2xl", "font-bold", "mb-2", "ml-2", "text-[#E2725B]");
+// productCategories.appendChild(categoryHeading);
 
 
 categories.forEach(category =>{
     const categoryContent = document.createElement("div");
-    categoryContent.innerHTML = `<button id="category-based-product-${category.id}" onclick="loadCategoryBasedProduct(${category.id})" class="hover:bg-[#E2725B] hover:text-white pt-2 pb-4 mb-2 ml-2  rounded-lg w-[120px] text-left">${category.category_name}</button>`;
+    categoryContent.innerHTML = `<button id="category-based-product-${category.id}" onclick="loadCategoryBasedProduct(${category.id})" class="bg-purple-100 hover:bg-pink-900 hover:text-white pt-2 pb-4  mb-1 ml-2  rounded-lg w-[150px] border border-gray-300">${category.category_name}</button>`;
 
     productCategories.appendChild(categoryContent);
 })
@@ -33,7 +33,7 @@ loadProductCategories();
 const loadAllMakeupProducts = ()=>{
     fetch("allProducts.json")
     .then(response =>response.json())
-    .then(data =>displayAllMakeupProducts(data.allMakeupProducts))
+    .then(data =>displayAllMakeupProducts(data.product))
 }
 
 const displayAllMakeupProducts = (allProducts)=>{
@@ -43,20 +43,18 @@ allMakeupProducts.innerHTML = "";
 
 allProducts.forEach(product =>{
     const productCard = document.createElement("div");
-    productCard.innerHTML = `<div class="card bg-base-100 w-96 shadow-lg flex flex-col flex-grow h-full min-h-[400px] justify-between transition-transform duration-300 ease-in-out hover:-translate-y-5 hover:shadow-lg">
-    <img class="h-[260px] pl-5 pr-5 pt-5" src="${product.image}">
-  <div class="card-body mb-[10px]">
+    productCard.innerHTML = `<div class="mb-6 card bg-base-100 w-85 shadow-lg h-full min-h-[350px] justify-between transition-transform duration-300 ease-in-out hover:-translate-y-5 hover:shadow-lg gap-6">
+    <img class="transition-transform duration-500 ease-in-out hover:scale-103 h-[250px] pl-5 pr-5 pt-5" src="${product.image}">
+  <div class="card-body mb-[5px]">
     <h2 class="card-title">${product.name}</h2>
     <p class="text-gray-400">${product.description}</p>
     <div class="flex justify-between">
     <button class="btn rounded-[10px]">৳${product.price}</button>
 
-<button class="btn btn-secondary bg-[#E2725B] group">
+<button class="btn btn-secondary bg-pink-900 border border-gray-100 group">
   <span class="group-hover:hidden">Add to Cart</span>
   <span class="hidden group-hover:block w-[74px]"><i class="fa-solid fa-cart-shopping"></i></span>
 </button>
-
-
     </div>
   </div>
 </div>`;
@@ -69,11 +67,12 @@ loadAllMakeupProducts();
 
 // category based products eyes
 const categoryFiles = {
-  1: "eyesCategory.json",
-  2: "FaceCategory.json",
-  3: "lipsCategory.json",
-  4: "nailsCategory.json",
-  5: "toolsCategory.json"
+  1: "allProducts.json",
+  2: "eyesCategory.json",
+  3: "FaceCategory.json",
+  4: "lipsCategory.json",
+  5: "nailsCategory.json",
+  6: "toolsCategory.json"
 };
 
 const loadCategoryBasedProduct = (id) => {
@@ -92,15 +91,15 @@ allEyesProduct.innerHTML = "";
 
 eyesProducts.forEach(eyesProduct =>{
     const eyesCard = document.createElement("div");
-    eyesCard.innerHTML = `<div class="card bg-base-100 w-96 shadow-lg flex flex-col flex-grow h-full min-h-[400px] justify-between transition-transform duration-300 ease-in-out hover:-translate-y-5 hover:shadow-lg">
-    <img class="h-[260px] pl-5 pr-5 pt-5" src="${eyesProduct.image}">
-  <div class="card-body mb-[10px]">
+    eyesCard.innerHTML = `<div class="mb-6 card bg-base-100 w-85 shadow-lg flex flex-col flex-grow h-full min-h-[350px] justify-between transition-transform duration-300 ease-in-out hover:-translate-y-5 hover:shadow-lg">
+    <img class="transition-transform duration-500 ease-in-out hover:scale-103 h-[250px] pl-5 pr-5 pt-5" src="${eyesProduct.image}">
+  <div class="card-body mb-[5px]">
     <h2 class="card-title">${eyesProduct.name}</h2>
     <p class="text-gray-400">${eyesProduct.description}</p>
     <div class="flex justify-between">
     <button class="btn rounded-[10px]">৳${eyesProduct.price}</button>
 
-<button class="btn btn-secondary bg-[#E2725B] group">
+<button class="btn btn-secondary bg-pink-900 border border-gray-100 group">
   <span class="group-hover:hidden">Add to Cart</span>
   <span class="hidden group-hover:block w-[74px]"><i class="fa-solid fa-cart-shopping"></i></span>
 </button>
